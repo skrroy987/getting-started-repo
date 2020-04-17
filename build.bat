97b94b7a1f467255486335ef99d71c84e0df8e54
skrroy987 <skr.saurabh90@gmail.com>
2020-04-17 09:05:59 +0530

    Update build.bat

--- build.bat
+++ build.bat
@@ -2,10 +2,10 @@
 
 echo Hello World
 
-FOR /l %%A in (1, 1, 20) DO (
-  ECHO Building [[01;31m %%A of 20 [00m]
-  PING 192.0.2.1 -n 1 -w 1000 >NUL
-)
+@FOR /l %%A in (1, 1, 20) DO (
+@  ECHO Building [[01;31m %%A of 20 [00m]
+@  PING 192.0.2.1 -n 1 -w 1000 >NUL
+@)
 
 (
   ECHO ^<html^>
