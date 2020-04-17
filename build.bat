ad3971d92414b6c8583cc578e3635a207ddea8a7
skrroy987 <skr.saurabh90@gmail.com>
2020-04-17 09:00:05 +0530

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 
-echo Let us pretend that this is building something ... and takes 20 seconds
+echo Hello World
 
 FOR /l %%A in (1, 1, 20) DO (
   ECHO Building [[01;31m %%A of 20 [00m]
